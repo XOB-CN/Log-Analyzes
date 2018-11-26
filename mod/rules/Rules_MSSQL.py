b40fb8629e57fdefc2c72e8387b2d5f84430a6d1
XOB-CN <748689323@qq.com>
2018-11-26 17:56:53 +0800

    v1.1.0

--- mod/rules/Rules_MSSQL.py
+++ mod/rules/Rules_MSSQL.py
@@ -9,13 +9,6 @@ RulesList = [
         'solution':"添加内存或限制SQL内存使用量"
     },
     {
-        'name': 'Security_login',
-        'type': 'Security',
-        'info': '登陆失败',
-        'keyword': "Login failed for user",
-        'solution': "请检查账户登陆相关的设置"
-    },
-    {
         'name': 'EventID_18056',
         'type': 'EventID',
         'info': 'The client was unable to reuse a session with SPID ***',
@@ -23,6 +16,15 @@ RulesList = [
         'solution':'考虑增加 IIS 连接池大小<br>'
                    '<a href="https://blog.csdn.net/yangzhawen/article/details/8209167">'
                    'https://blog.csdn.net/yangzhawen/article/details/8209167</a>'
+    },
+    {
+        'name': 'EventID_18456',
+        'type': 'EventID',
+        'info': '登陆失败',
+        'keyword': "Error: 18456",
+        'solution': '请检查账户登陆相关的设置<br>'
+                    '<a href="https://docs.microsoft.com/en-us/sql/relational-databases/errors-events/mssqlserver-18456-database-engine-error?view=sql-server-2017">'
+                    'https://docs.microsoft.com/en-us/sql/relational-databases/errors-events/mssqlserver-18456-database-engine-error?view=sql-server-2017</a>'
     },
     {
         'name': 'EventID_26073',
