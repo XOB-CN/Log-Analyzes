a44c5c7ef37c6a688250870ffdad8fae5ae63231
XOB-CN <748689323@qq.com>
2019-01-29 18:11:25 +0800

    Update AnalysisRules_ITOM_OA.py
    更新规则

--- mod/rules/AnalysisRules_ITOM_OA.py
+++ mod/rules/AnalysisRules_ITOM_OA.py
@@ -4,33 +4,51 @@
 # 注意：如果想要匹配 < 或 >, 则匹配的规则需要替换为 &lt; 或 &gt;
 match_rules_list = [
     {
-        'name': 'OPC NodeName',
+        'name': 'OA NodeName',
         'type': 'Information',
         'match': "OPC_NODENAME",
-        'rule': 'line.split("=")[1]'
+        'rule': 'line.split("=")[-1]',
+    },
+    {
+        'name': 'OA Version',
+        'type': 'Information',
+        'match': "OPC_INSTALLED_VERSION",
+        'rule': 'line.split("=")[-1]',
+    },
+    {
+        'name': 'OA IP Address',
+        'type': 'Information',
+        'match': "OPC_IP_ADDRESS",
+        'rule': 'line.split("=")[-1]',
+    },
+    {
+        'name': 'OS Type',
+        'type': 'Information',
+        'match': "ostype=",
+        'rule': 'line.split("=")[-1]',
     },
     {
         'name': 'Certificate Server',
         'type': 'Information',
         'match': "CERTIFICATE_SERVER",
-        'rule': 'line.split("=")[1]'
+        'rule': 'line.split("=")[-1]',
     },
     {
         'name': 'OpC30-797 - Policies cannot connect to coda',
         'type': 'OA Error ID',
         'match': "OpC30-797",
-        'solution': '请参考 KM1208518'
+        'solution': '请参考 KM1208518',
     },
     {
         'name': '其余 Warn 信息',
         'type': 'Others',
         'match': "Wrn",
-        'rule': "line"
+        'rule': "line",
     },
     {
         'name': '其余 Error 信息',
         'type': 'Others',
         'match': "Err",
-        'rule': "line"
+        'rule': "line",
     },
 ]
