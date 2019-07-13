46191a2cc29e3fa31eb0057bb9a20ecaaa666c3e
XOB-CN <748689323@qq.com>
2019-07-13 20:13:41 +0800

    3.2.0 Beta v7

--- bin/demo_mongo.py
+++ bin/demo_mongo.py
@@ -1,23 +1,20 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from mod.tools.io_mongo import MongoDB
 
 sess = MongoDB()
-mydb = sess.get_mongo_sess('20190708220011', 'system_txt')
+mydb = sess.get_mongo_sess('20190713131741', 'default')
 
 # 将 MongoDB 的数据转换成 Pandas 的数据
 mo_data = mydb.find({'$or':[{'log_level':'WRN'},{'log_level':'ERR'}]})
 pd_data = pd.DataFrame(list(mo_data))
 del pd_data['_id']
 
-# 开始构造最终数据
-data_index = pd_data.log_time
-data = pd.Series(np.random.randint(1,2,len(data_index)), index=data_index)
-data = data.resample('T').sum()
-data = data[data.values > 0]
-print(data)
+# 开始处理 pandas 数据
+tmp1_data = pd_data.set_index('log_time')
+tmp2_data = tmp1_data.log_weight.resample('T').sum()
+tmp3_data = tmp2_data[tmp2_data.values > 0]
 
-# 开始绘图
-plt.bar(data.index, data.values)
+# 绘制图表：基于时间的事件总数
+plt.bar(tmp3_data.index, tmp3_data)
 plt.show()
