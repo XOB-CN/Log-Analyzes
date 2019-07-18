39d3d71126ce30bd13eb051fe9a840cd2cad471a
XOB-CN <748689323@qq.com>
2019-07-18 18:01:37 +0800

    v3.2.2 beta v2

--- mod/analysis/itom_obm.py
+++ mod/analysis/itom_obm.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-import re
+import re, os
 from datetime import datetime
 from mod.tools.match import Match
 
@@ -12,6 +12,9 @@ def analysis_to_mongodb(Queue_Input, Queue_Output, black_list):
     fin_list = []
     fin_data = []
 
+    # 初始化数据变量
+    file_type = None
+
     # 从 Queue 中获取数据
     while n:
         queue_data = Queue_Input.get()
@@ -19,8 +22,6 @@ def analysis_to_mongodb(Queue_Input, Queue_Output, black_list):
             n = False
         else:
             # 如果获取的数据类型是 logs, 则进行进一步的处理
+            # queue_data: {'section_id': id, 'type': 'log', 'filepath': filepath, 'log_content': [行数, 日志内容]}
             if queue_data.get('type') == 'logs':
-                filepath = queue_data.get('filepath')
-                log_file = filepath
-            for i in queue_data:
-                print(i)
+                file_type = os.path.basename(queue_data.get('filepath')).split('.')[0]
